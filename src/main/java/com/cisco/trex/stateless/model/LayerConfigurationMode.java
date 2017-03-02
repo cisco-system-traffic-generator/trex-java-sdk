902d571df40e3c59e7a5d64bb08ab2510663e6df
Vyacheslav Ogai <vyacheslav.ogai@gmail.com>
2017-03-03 00:29:06 +0700

    Add getEtherAttrs and getIpv4Attrs

--- src/main/java/com/cisco/trex/stateless/model/LayerConfigurationMode.java
+++ src/main/java/com/cisco/trex/stateless/model/LayerConfigurationMode.java
@@ -8,4 +8,14 @@ public class LayerConfigurationMode {
     public LayerConfigurationMode(JsonObject attrs) {
         this.attrs = attrs;
     }
+    
+    public String getEtherAttr(String attrName) {
+        JsonObject ether = attrs.get("ether").getAsJsonObject();
+        return ether != null ? ether.get(attrName).getAsString() : null;
+    }
+    
+    public String getIpv4Attr(String attrName) {
+        JsonObject ipV4 = attrs.get("ipv4").getAsJsonObject();
+        return ipV4 != null ? ipV4.get(attrName).getAsString() : null;
+    }
 }
