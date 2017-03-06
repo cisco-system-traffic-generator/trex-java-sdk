3984816e3dcfa8854cf60e1afc6baf835f5fbdc3
Vyacheslav Ogai <vyacheslav.ogai@gmail.com>
2017-03-06 20:55:26 +0700

    Remove logging implementation in SDK. Added logger to example.

--- examples/bootstrap/src/main/java/com/cisco/trex/stateless/TRexClientApp.java
+++ examples/bootstrap/src/main/java/com/cisco/trex/stateless/TRexClientApp.java
@@ -3,6 +3,8 @@ package com.cisco.trex.stateless;
 import com.cisco.trex.stateless.exception.TRexConnectionException;
 import com.cisco.trex.stateless.exception.TRexTimeoutException;
 import com.cisco.trex.stateless.model.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -14,7 +16,8 @@ public class TRexClientApp {
     public static final String client_name = "trex-test-user";
     public static final String trex_host = "trex.mycompany.com";
     public static final String trex_port = "4501";
-
+    private static Logger logger = LoggerFactory.getLogger(TRexClientApp.class);
+    
     // API usage example. see also:
     // https://github.com/cisco-system-traffic-generator/trex-java-sdk/blob/master/src/test/java/com/cisco/trex/stateless/TRexClientTest.java
 
@@ -24,8 +27,9 @@ public class TRexClientApp {
         List<Port> ports = client.getPorts();
         System.out.println("Found " + ports.size() + " ports");
         List<String> cmds = client.getSupportedCommands();
+        logger.info("List of available commands:");
         for (String cmd: cmds) {
-            System.out.println(" - " + cmd);
+            logger.info(" - " + cmd);
         }
         client.disconnect();
     }
