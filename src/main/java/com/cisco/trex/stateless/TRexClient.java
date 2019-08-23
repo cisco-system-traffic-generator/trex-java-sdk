5477df14b902559e3de01d8e362b1a31a2210568
Emil Gustafsson <emil.gustafsson@ericsson.com>
2019-08-23 12:24:25 +0200

    format TRexClient.java
    Signed-off-by: Emil Gustafsson <emil.gustafsson@ericsson.com>

--- src/main/java/com/cisco/trex/stateless/TRexClient.java
+++ src/main/java/com/cisco/trex/stateless/TRexClient.java
@@ -23,7 +23,6 @@ import com.google.common.collect.Lists;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
