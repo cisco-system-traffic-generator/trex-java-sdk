790dabe05fe49bd549667d19ff691fa77b480a4f
Emil Gustafsson <31921952+Emil-Gustafsson@users.noreply.github.com>
2019-09-09 08:23:28 +0200

    catch zmq exception when no respons from server
    add more info when there is no respons from server
    add javadoc

--- src/main/java/com/cisco/trex/stateless/TRexTransport.java
+++ src/main/java/com/cisco/trex/stateless/TRexTransport.java
@@ -11,43 +11,55 @@ import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.zeromq.ZMQ;
+import org.zeromq.ZMQException;
 
+/** TRex Transport class to create zmq socket for connection to trex server */
 public class TRexTransport {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(TRexTransport.class);
-
-  public static final int DEFAULT_TIMEOUT = 3000;
-
+  private static final int DEFAULT_TIMEOUT = 3000;
+  private static final String PROTOCOL = "tcp";
   private final String connectionString;
-
   private final IDataCompressor dataCompressor;
+  private final ZMQ.Context zmqCtx = ZMQ.context(1);
+  private final ZMQ.Socket zmqSocket;
+  private final String host;
+  private final String port;
 
-  private ZMQ.Context zmqCtx = ZMQ.context(1);
-
-  private ZMQ.Socket zmqSocket;
-
-  private String protocol = "tcp";
-
-  private String host;
-
-  private String port;
-
+  /**
+   * @param host Server address
+   * @param port Server port
+   * @param timeout How long to wait for server response
+   * @param dataCompressor
+   */
   public TRexTransport(String host, String port, int timeout, IDataCompressor dataCompressor) {
     this.host = host;
     this.port = port;
-    zmqSocket = zmqCtx.socket(ZMQ.REQ);
+    this.zmqSocket = zmqCtx.socket(ZMQ.REQ);
     int actualTimeout = timeout <= 0 ? DEFAULT_TIMEOUT : timeout;
     zmqSocket.setReceiveTimeOut(actualTimeout);
     zmqSocket.setSendTimeOut(actualTimeout);
-    connectionString = protocol + "://" + this.host + ":" + this.port;
+    this.connectionString = PROTOCOL + "://" + this.host + ":" + this.port;
     zmqSocket.connect(connectionString);
     this.dataCompressor = dataCompressor;
   }
 
+  /**
+   * @param host Server address
+   * @param port Server port
+   * @param timeout How long to wait for server response
+   */
   public TRexTransport(String host, String port, int timeout) {
     this(host, port, timeout, new TRexDataCompressor());
   }
 
+  /**
+   * Send TRex command to the server
+   *
+   * @param command
+   * @return RPCResponse
+   * @throws IOException
+   */
   public RPCResponse sendCommand(TRexCommand command) throws IOException {
     String json = new ObjectMapper().writeValueAsString(command.getParameters());
     String response = sendJson(json);
@@ -60,6 +72,13 @@ public class TRexTransport {
     return objectMapper.readValue(response, RPCResponse.class);
   }
 
+  /**
+   * Send TRex command list to the server
+   *
+   * @param commands
+   * @return RPCResponse
+   * @throws IOException
+   */
   public RPCResponse[] sendCommands(List<TRexCommand> commands) throws IOException {
     if (commands.size() == 1) {
       return new RPCResponse[] {sendCommand(commands.get(0))};
@@ -77,34 +96,53 @@ public class TRexTransport {
     return new ObjectMapper().readValue(response, RPCResponse[].class);
   }
 
+  /** @return server host address */
   public String getHost() {
     return host;
   }
 
+  /** @return server port */
   public String getPort() {
     return port;
   }
 
+  /** close zmq connection to server */
   public synchronized void close() {
     zmqSocket.disconnect(connectionString);
     zmqSocket.close();
     zmqCtx.close();
   }
 
+  /** @return zmq socket */
   public ZMQ.Socket getSocket() {
     return zmqSocket;
   }
 
+  /**
+   * Send json string to server
+   *
+   * @param json
+   * @return json string
+   */
   public synchronized String sendJson(String json) {
-    LOGGER.debug("JSON Req: " + json);
+    LOGGER.debug("JSON Req: {}", json);
 
     byte[] compressed = this.dataCompressor.compressStringToBytes(json);
 
-    zmqSocket.send(compressed);
+    try {
+      zmqSocket.send(compressed);
+    } catch (ZMQException e) {
+      throw new IllegalStateException(
+          "Did not get any response from server "
+              + getHost()
+              + " within timeout "
+              + zmqSocket.getReceiveTimeOut(),
+          e);
+    }
     byte[] msg = zmqSocket.recv();
 
     String response = this.dataCompressor.decompressBytesToString(msg);
-    LOGGER.debug("JSON Resp: " + response);
+    LOGGER.debug("JSON Resp: {}", response);
     return response;
   }
 }
