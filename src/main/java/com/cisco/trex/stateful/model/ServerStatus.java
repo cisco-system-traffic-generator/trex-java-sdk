c0b2513cfcd5e92953550e701b6b851f6cb508ba
Leo Ma <leo.ma@ericsson.com>
2019-10-09 16:32:41 +0800

    format changed files by google formatter
    Signed-off-by: Leo Ma <leo.ma@ericsson.com>

--- src/main/java/com/cisco/trex/stateful/model/ServerStatus.java
+++ src/main/java/com/cisco/trex/stateful/model/ServerStatus.java
@@ -1,67 +1,66 @@
 package com.cisco.trex.stateful.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.Map;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public class ServerStatus {
-	
-	public enum State {
-		@JsonProperty("0")
-		IDLE,
-		
-		@JsonProperty("1")
-		LOADED,
-		
-		@JsonProperty("2")
-		PARSING,
-		
-		@JsonProperty("3")
-		BUILDING,
-		
-		@JsonProperty("4")
-		TRANSMITTING,
-		
-		@JsonProperty("5")
-		CLEANUP;
-	}
 
-	@JsonProperty("epoch")
-	private int epoch;
-	
-	@JsonProperty("state")
-	private int state;
+  public enum State {
+    @JsonProperty("0")
+    IDLE,
 
-	@JsonProperty("state_profile")
-	private Map<String, State> stateProfile;
+    @JsonProperty("1")
+    LOADED,
 
-	@JsonProperty("epoch")
-	public int getEpoch() {
-		return epoch;
-	}
+    @JsonProperty("2")
+    PARSING,
 
-	@JsonProperty("epoch")
-	public void setEpoch(int epoch) {
-		this.epoch = epoch;
-	}
+    @JsonProperty("3")
+    BUILDING,
 
-	@JsonProperty("state")
-	public int getState() {
-		return state;
-	}
+    @JsonProperty("4")
+    TRANSMITTING,
 
-	@JsonProperty("state")
-	public void setState(int state) {
-		this.state = state;
-	}
+    @JsonProperty("5")
+    CLEANUP;
+  }
 
-	@JsonProperty("state_profile")
-	public Map<String, State> getStateProfile() {
-		return stateProfile;
-	}
+  @JsonProperty("epoch")
+  private int epoch;
 
-	@JsonProperty("state_profile")
-	public void setStateProfile(Map<String, State> stateProfile) {
-		this.stateProfile = stateProfile;
-	}
+  @JsonProperty("state")
+  private int state;
+
+  @JsonProperty("state_profile")
+  private Map<String, State> stateProfile;
+
+  @JsonProperty("epoch")
+  public int getEpoch() {
+    return epoch;
+  }
+
+  @JsonProperty("epoch")
+  public void setEpoch(int epoch) {
+    this.epoch = epoch;
+  }
+
+  @JsonProperty("state")
+  public int getState() {
+    return state;
+  }
+
+  @JsonProperty("state")
+  public void setState(int state) {
+    this.state = state;
+  }
+
+  @JsonProperty("state_profile")
+  public Map<String, State> getStateProfile() {
+    return stateProfile;
+  }
+
+  @JsonProperty("state_profile")
+  public void setStateProfile(Map<String, State> stateProfile) {
+    this.stateProfile = stateProfile;
+  }
 }
