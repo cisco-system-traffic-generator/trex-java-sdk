4eb0ee616c4e3debee42f59af395f34b17244d41
Emil Gustafsson <emil.gustafsson@ericsson.com>
2019-11-04 08:07:43 +0100

    update and test multicastMacFromIPv6
    Signed-off-by: Emil Gustafsson <emil.gustafsson@ericsson.com>

--- src/test/java/com/cisco/trex/stateless/IPv6NeighborDiscoveryServiceTest.java
+++ src/test/java/com/cisco/trex/stateless/IPv6NeighborDiscoveryServiceTest.java
@@ -3,6 +3,7 @@ package com.cisco.trex.stateless;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.pcap4j.util.MacAddress;
 
 public class IPv6NeighborDiscoveryServiceTest {
 
@@ -10,5 +11,12 @@ public class IPv6NeighborDiscoveryServiceTest {
   public void generateIPv6AddrFromMACTest() {
     String ipV6Address = IPv6NeighborDiscoveryService.generateIPv6AddrFromMAC("10:62:E5:09:A0:64");
     assertEquals("fe80::1262:e5ff:fe09:a064", ipV6Address);
+  }
+
+  @Test
+  public void generateMulticastMacFromIPv6Test() {
+    MacAddress multicastMac =
+        IPv6NeighborDiscoveryService.multicastMacFromIPv6("fe80::4a5d:60ff:fee8:658f");
+    assertEquals("33:33:ff:e8:65:8f", multicastMac.toString());
   }
 }
